044fadf709640988ab821c314c0250604cca7d38
sunhpark42 <sunhpark42@gmail.com>
2021-05-17 16:02:46 +0900

    feat: 결과페이지 와이어 프레임 잡기

--- react_app/src/pages/ResultPage.js
+++ react_app/src/pages/ResultPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router';
+import { PATH } from '../constants';
 import result from '../data/result';
 
 const ResultPage = () => {
+  const history = useHistory();
   const { id } = useParams();
-  const { name, description } = result.find((res) => res.id === Number(id));
+  const { image, name, description } = result.find((res) => res.id === Number(id));
 
-  return <div>{(name, description)}</div>;
+  const onRestart = () => {
+    history.push(PATH.ROOT);
+  };
+
+  return (
+    <div>
+      {name}
+      <img src={image} alt={name} width="100%" />
+      {description}
+      <button type="button" onClick={onRestart}>
+        다시시작
+      </button>
+    </div>
+  );
 };
 
 export default ResultPage;
